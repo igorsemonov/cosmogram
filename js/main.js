3d7637de0b7d6c19c23c4f848794c2612df4749a
igorsemonov <newstudy2424@gmail.com>
2024-12-29 17:56:23 +0200

    add photo rendering function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 
-const obj = () => ({
-    id: getRandomInt(1, 25),
-    url: getUrl(),
+const names = ['John', 'Bill', 'Jane', 'Alice', 'Steve', 'Lara'];
+
+const descriptions = ["Sun", "Beach", "Mountain", "River", "City", "Club", "Night", "Moon"];
+
+const messages = ['Все відмінно!',
+    'Загалом все непогано. Але не всі.',
+    'Коли ви робите фотографію, добре б прибирати палець із кадру. Зрештою, це просто непрофесійно.',
+    'Моя бабуся випадково чхнула з фотоапаратом у руках і у неї вийшла фотографія краща.',
+    'Я послизнувся на банановій шкірці і впустив фотоапарат на кота і у мене вийшла фотографія краще.',
+    'Обличчя людей на фотці перекошені, ніби їх побивають. Як можна було зловити такий невдалий момент?'
+    ];
+
+const obj = (idx) => ({
+    id: idx+1,
+    url: getUrl(idx),
     likes: getRandomInt(15, 200),
-    description: getDescription(),
+    description: getRandomElement(descriptions),
     comments: getComments()
 });
 
@@ -11,44 +23,43 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function getUrl() {
-    return `photos/${getRandomInt(1, 6)}.jpg`;
+function getUrl(idx) {
+    return `photos/${idx+1}.jpg`;
 };
 
 function getAvatar() {
     return `img/avatar-${getRandomInt(1, 6)}.svg`;
 };
 
-function getName() {
-    const names = ['John', 'Bill', 'Jane', 'Alice', 'Steve', 'Lara'];
-    return names[Math.floor(Math.random() * names.length)];
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
 };
-
-function getDescription() {
-    const descriptions = ["Sun", "Beach", "Mountain", "River", "City", "Club", "Night", "Moon"];
-    return descriptions[Math.floor(Math.random() * descriptions.length)];
-};
-
-function getMessage() {
-    const messages = ['Все відмінно!',
-        'Загалом все непогано. Але не всі.',
-        'Коли ви робите фотографію, добре б прибирати палець із кадру. Зрештою, це просто непрофесійно.',
-        'Моя бабуся випадково чхнула з фотоапаратом у руках і у неї вийшла фотографія краща.',
-        'Я послизнувся на банановій шкірці і впустив фотоапарат на кота і у мене вийшла фотографія краще.',
-        'Обличчя людей на фотці перекошені, ніби їх побивають. Як можна було зловити такий невдалий момент?'
-        ];
-    return messages[Math.floor(Math.random() * messages.length)];
-}
 
 function getComments() {
-    return Array.from({length: getRandomInt(1, 5)}, () => ({
-        id: getRandomInt(1, 200),
+    return Array.from({length: getRandomInt(1, 5)}, (_,index) => ({
+        id: ++index,
         avatar: getAvatar(),
-        message: getMessage(),
-        name: getName()
+        message: getRandomElement(messages),
+        name: getRandomElement(names)
     }));
 };
 
-const photos = Array.from({length: 25}, obj);
+const photos = Array.from({length: 25}, (_,idx)=>obj(idx));
+console.log(photos)
 
-console.log(photos);
+// ====================================================================================================
+
+function renderAllPhotos(arr) {
+
+    const template = document.querySelector('#picture');
+    const uploadSection = document.querySelector('.pictures');
+
+    arr.forEach(elem => {   
+        let clonedTemplate = template.content.cloneNode(true);
+        clonedTemplate.querySelector('.picture__img').src = elem.url;
+        clonedTemplate.querySelector('.picture__likes').textContent = elem.likes;
+        clonedTemplate.querySelector('.picture__comments').textContent = elem.comments.length;
+        uploadSection.appendChild(clonedTemplate);
+});
+};
+renderAllPhotos(photos);
